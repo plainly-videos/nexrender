76b16096480275b2d7e0de9f96f9733e4a6a1125
Daniel Elero <danixeee@gmail.com>
2020-12-02 18:06:14 +0100

    Add plainly provider

--- packages/nexrender-core/src/index.js
+++ packages/nexrender-core/src/index.js
@@ -31,6 +31,7 @@ if (process.env.NEXRENDER_REQUIRE_PLUGINS) {
     require('@nexrender/provider-s3');
     require('@nexrender/provider-ftp');
     require('@nexrender/provider-gs');
+    require('@nexrender/provider-plainly');
 }
 
 //
