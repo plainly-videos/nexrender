9050adf3e90adb36f426ab441e0a892d92bbe738
Daniel Elero <danixeee@gmail.com>
2021-05-07 17:57:37 +0200

    Action upload glob support

--- packages/nexrender-action-upload/index.js
+++ packages/nexrender-action-upload/index.js
@@ -1,9 +1,10 @@
 const fs       = require('fs')
 const path     = require('path')
 const {name}   = require('./package.json')
+const globFn   = require('glob')
 const requireg = require('requireg')
 
-module.exports = (job, settings, { input, provider, params, ...options }, type) => {
+module.exports = (job, settings, { input, provider, params, glob, ...options }, type) => {
     let onProgress;
     let onComplete;
 
@@ -29,30 +30,64 @@ module.exports = (job, settings, { input, provider, params, ...options }, type) 
         onComplete = (job, file) => options.onComplete(job, file);
     }
 
-    settings.logger.log(`[${job.uid}] starting action-upload action`)
+    settings.logger.log(`[${job.uid}] starting action-upload action`);
 
-    let requirePackage = ''
-    try {
-        /* try requiring official providers */
-        requirePackage = `@nexrender/provider-${provider}`
-        return requireg(requirePackage).upload(job, settings, input, params || {}, onProgress, onComplete);
+    return new Promise((resolve) => {
 
-    } catch (e) {
-        if (e.message.indexOf('Could not require module') !== -1) {
-            try{
-                /* try requiring custom providers */
-                requirePackage = provider
-                return requireg(requirePackage).upload(job, settings, input, params || {}, onProgress, onComplete);
+      if (glob) { // Search files that match glob
 
-            } catch(e) {
-                if (e.message.indexOf('Could not require module') !== -1) {
-                    return Promise.reject(new Error(`Couldn\'t find module ${requirePackage}, Unknown provider given.`))
-                }
+        settings.logger.log(`[${job.uid}] action-upload: Matching paths for: ${input}`);
 
-                throw e;
+        globFn(input, (err, matches) => {
+          if (err) throw err;
+
+          if (matches.length === 0) return resolve(job);
+
+          matches.forEach((absFilePath, index) => {
+            const filePath = path.relative(job.workpath, absFilePath);
+
+            if (params) {
+              params['glob'] = true;
+              params['filePath'] = filePath;
             }
-        }
 
-        throw e;
-    }
+            doUpload(job, provider, settings, absFilePath, params, onProgress, onComplete).then(() => {
+              if (index === matches.length - 1) resolve(job);
+            });
+          })
+        });
+
+      } else {  // Upload single file
+        doUpload(job, provider, settings, input, params, onProgress, onComplete).then(resolve);
+      }
+
+    });
+}
+
+const doUpload = (job, provider, settings, input, params, onProgress, onComplete) => {
+  let requirePackage = ''
+
+  try {
+      /* try requiring official providers */
+      requirePackage = `@nexrender/provider-${provider}`
+      return requireg(requirePackage).upload(job, settings, input, params || {}, onProgress, onComplete);
+
+  } catch (e) {
+      if (e.message.indexOf('Could not require module') !== -1) {
+          try{
+              /* try requiring custom providers */
+              requirePackage = provider
+              return requireg(requirePackage).upload(job, settings, input, params || {}, onProgress, onComplete);
+
+          } catch(e) {
+              if (e.message.indexOf('Could not require module') !== -1) {
+                  return Promise.reject(new Error(`Couldn\'t find module ${requirePackage}, Unknown provider given.`))
+              }
+
+              throw e;
+          }
+      }
+
+      throw e;
+  }
 }
